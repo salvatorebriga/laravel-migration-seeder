531b5420db9417086675f07812b6bf2e49e64bdf
salvatorebriga <salvo.z2302@gmail.com>
2024-07-09 22:42:15 +0200

    visualization

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Template</title>
+    @vite('resources/js/app.js')
+  </head>
+
+  <body class="bg-dark text-light">
+    {{-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template"> --}}
+
+    <div class="container text-center">
+      <header class="py-3">
+        <h1>Treni</h1>
+      </header>
+      <main>
+        @yield('main')
+      </main>
+      <footer>
+        Sono il footer
+      </footer>
+    </div>
+
+
+
+  </body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
-    @vite('resources/js/app.js')
-</head>
+@section('main')
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
 
-<body>
-    <h1>
-        Prova
-    </h1>
-
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
-
-</body>
-
-</html>
+    @foreach ($trains as $train)
+      <div class="col">
+        <div class="card mb-5 p-4">
+          <p><strong>Azienda: </strong> {{ $train->azienda }}</p>
+          <p><strong>Partenza:</strong> {{ $train->stazione_di_partenza }}</p>
+          <p><strong>Arrivo:</strong> {{ $train->stazione_di_arrivo }}</p>
+          <p><strong>Codice Treno:</strong> {{ $train->codice_treno }}</p>
+          <p><strong>Numero Carrozze:</strong> {{ $train->numero_carrozze }}</p>
+          <p><strong>In Orario:</strong> {{ $train->in_orario ? 'Sì' : 'No' }}</p>
+          <p><strong>Cancellato:</strong> {{ $train->cancellato ? 'Sì' : 'No' }}</p>
+          <p><strong>Creato il:</strong> {{ $train->created_at }}</p>
+          <p><strong>Aggiornato il:</strong> {{ $train->updated_at }}</p>
+        </div>
+      </div>
+    @endforeach
+    </ul>
+  </div>
+@endsection

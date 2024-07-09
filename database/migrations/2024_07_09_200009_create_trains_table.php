a5555c6a05cb755db6127f9377faf82ee3a4c38f
salvatorebriga <salvo.z2302@gmail.com>
2024-07-09 22:43:03 +0200

    migration

---
+++ database/migrations/2024_07_09_200009_create_trains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyText('azienda');
+            $table->tinyText('stazione_di_partenza');
+            $table->tinyText('stazione_di_arrivo');
+            $table->smallInteger('codice_treno');
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};

28a7e58ebaf118912a8cc61707c2df330700c96c
salvatorebriga <salvo.z2302@gmail.com>
2024-07-09 23:17:53 +0200

    query modificata

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $today = Carbon::now()->format('Y-m-d');
+
+        $trains = Train::whereDate('partenza', '>=', $today)
+            ->orderBy('partenza', 'asc')
+            ->get();
         return view('welcome', compact('trains'));
     }
 }

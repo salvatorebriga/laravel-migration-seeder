c348c7017e1761437e7d411350716c24428cce40
salvatorebriga <salvo.z2302@gmail.com>
2024-07-10 17:28:26 +0200

    optimization

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,14 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'azienda',
-        'stazione_di_partenza',
-        'stazione_di_arrivo',
-        'codice_treno',
-        'numero_carrozze',
-        'in_orario',
-        'cancellato',
-    ];
 }
